facf19621749fe13f63eae32ab12110676462885
vetletm <vetlemoen@gmail.com>
2021-01-28 15:59:40 +0100

    add clang-6.0 to required packages

--- bin/install-p4dev-with-p4ovs.sh
+++ bin/install-p4dev-with-p4ovs.sh
@@ -688,7 +688,7 @@ echo "------------------------------------------------------------"
 echo "Start installing P4-OvS and its dependencies"
 set +x
 
-sudo apt-get install -y automake cmake libjudy-dev libgmp-dev libpcap-dev libboost-all-dev libevent-dev libtool flex bison pkg-config g++ libssl-dev libnanomsg-dev libjudy-dev libreadline-dev valgrind libtool-bin libboost-dev libboost-system-dev libboost-thread-dev
+sudo apt-get install -y automake cmake libjudy-dev libgmp-dev libpcap-dev libboost-all-dev libevent-dev libtool flex bison pkg-config g++ libssl-dev libnanomsg-dev libjudy-dev libreadline-dev valgrind libtool-bin libboost-dev libboost-system-dev libboost-thread-dev clang-6.0
 
 sudo apt-get install -y python3-pip python3-dev
 sudo pip3 install nnpy
